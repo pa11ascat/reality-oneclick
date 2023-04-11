a1837981ea0ca979fe296014be2b169f06e03cca
Mohammad Massoudi <m.massoudi32@gmail.com>
2023-04-11 04:14:48 +0330

    making reality ping get urls repeatedly

--- reality.py
+++ reality.py
@@ -1,18 +1,24 @@
 import os
-import subprocess
+from enum import Enum
+
+class Menu(Enum):
+    EXIT = 0
+    CONFIG = 1
+    RPING = 2
+    EXPORT = 3
+    HOME = -1
 
 def menu()-> int:
     menu_desc = '''
+    ----------------------------
     1.Configuration
 
     2.Reality ping
 
     3.Export Links
 
-    ___________________________
-
     0.Exit
-
+    ___________________________
 
     '''
     print(menu_desc)
@@ -21,25 +27,39 @@ def menu()-> int:
 
     return num
 
+def reality_ping():
+    print("----------------------------")
+    domain = str(input("Enter Your Domain(e.g. digikala.com, -1 to exit): "))
+    if domain == "-1":
+        return Menu.HOME
+    else: 
+        result = os.system("xray tls ping " + domain)        
+        print(result)
+        return Menu.RPING
+
 def configuration():
-    pass
+    return Menu.HOME
+
+choice = Menu.HOME
 
 while (True):
-    choice = menu()
+    if choice == Menu.HOME:
+        choice = menu()
+    
     match choice:
-        case 1:
+        case Menu.CONFIG:
             print("Option 1 selected")
-        case 2:
-            os.system("clear")
-            domain = str(input("Enter Your Domain(e.g. digikala.com): "))
-            result = os.system("xray tls ping " + domain)
-            print(result)
-        case 3:
+            choice = configuration()
+        case Menu.RPING:
+            choice = reality_ping()
+        case Menu.HOME:
             print("Option 3 selected")
+            choice = Menu.HOME
         case 0:
             print("Exiting program")
             break
         case _:
             print("wrong value selected")
+            choice = Menu.HOME
 
         
