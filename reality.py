08dc22fc80c7b2652faf13dd5996b0da22101380
5ur Rea1ist <tlbyn.amir@gmail.com>
2023-04-11 07:22:57 +0330

    finishig configurstion method and adding some lines to setup.sh

--- reality.py
+++ reality.py
@@ -1,5 +1,11 @@
 import os
 from enum import Enum
+from uuid import UUID
+
+#define some const var
+KEYS_LENGTH=43
+PRIV_KEY_START=13
+SHORT_ID_NUM=1
 
 class Menu(Enum):
     EXIT = 0
@@ -8,7 +14,28 @@ class Menu(Enum):
     EXPORT = 3
     HOME = -1
 
-def menu()-> int:
+
+def is_valid_uuid(uuid_to_test:str, version: int=4) -> bool:
+    try:
+        uuid_obj = UUID(uuid_to_test, version=version)
+    except ValueError:
+        return False
+    return str(uuid_obj) == uuid_to_test
+
+
+def magic_string_maker(string: str) -> (str, int):
+    string_list = string.split(",")
+    result = ''
+    counter = 0
+    for counter in range(len(string_list)):
+        if counter == len(string_list)-1:
+            result = result + '"' + string_list[counter] + '"'
+        else:
+            result = result + '"' + string_list[counter] + '",\n'
+    return result, len(string_list)+1
+
+
+def menu() -> int:
     menu_desc = '''
     ----------------------------
     1.Configuration
@@ -37,7 +64,7 @@ def menu()-> int:
 
 def reality_ping():
     print("----------------------------")
-    domain = str(input("Enter Your Domain(e.g. digikala.com, -1 to exit): "))
+    domain = str(input("Enter Your Domain(e.g. debian.org, -1 to exit): "))
     if domain == "-1":
         return Menu.HOME
     else: 
@@ -46,7 +73,59 @@ def reality_ping():
         return Menu.RPING
 
 def configuration():
-    return Menu.HOME
+    os.system("clear")
+    #uuid4 setup
+    uuid4_vision = input("Import your uuid4 for vision(enter for generating one): ")
+    if not(is_valid_uuid(uuid4_vision)):
+        uuid4_vision = os.system("xray uuid4")
+        print(uuid4_vision+"\n")
+    uuid4_h2 = input("Import your uuid4 for h2(enter for generating one): ")
+    if not(is_valid_uuid(uuid4_h2)):
+        uuid4_h2 = os.system("xray uuid4")
+        print(uuid4_h2+"\n")
+    
+    
+    #generating key
+    keys = os.system("xray x25519")
+    priv_key = keys[PRIV_KEY_START:PRIV_KEY_START+KEYS_LENGTH]
+    pub_key = keys[-KEYS_LENGTH,-1]
+    os.system(f"echo {keys} > key")
+
+    #getting dest and serverNames
+
+    dest_domain = input("Import dest domain: ")
+
+    serverNames_str = input("Import Allowed domains(e.g. debian.org,ftp.debian.org): ")
+
+    serverNames = magic_string_maker(serverNames_str)
+
+    #short Id
+
+    short_ids_str = input("Import your shortIds(seperated by comma e.g. ss2,dfaf3 or type how many you want and we generate it for you e.g. 5): ")
+
+    shid_str = ""
+
+    if short_ids_str.isnumeric():
+        for i in range(int(short_ids_str)):
+            shid = os.system("openssl rand -hex 8")
+            if i == int(short_ids_str)-1:
+                shid_str += f"{shid}"
+            else:
+                shid_str += f"{shid},"
+        
+        short_ids, SHORT_ID_NUM = magic_string_maker(shid_str)
+            
+    else:
+        short_ids, SHORT_ID_NUM = magic_string_maker(short_ids_str)
+
+    # download config.json
+
+    #edit config.json
+
+
+def export() -> str:
+    pass
+
 
 choice = Menu.HOME
 
@@ -56,14 +135,13 @@ while (True):
     
     match choice:
         case Menu.CONFIG:
-            print("Option 1 selected")
             choice = configuration()
         case Menu.RPING:
             choice = reality_ping()
-        case Menu.HOME:
+        case Menu.EXPORT:
             print("Option 3 selected")
             choice = Menu.HOME
-        case 0:
+        case Menu.EXIT:
             print("Exiting program")
             break
 
--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 # reality_setup.sh
 
+#install git
+echo "Check if a git environment is included"
+if ! [ -x "$(command -v git)" ]; then
+  echo 'no git environment, installing git... '  >&2
+  sudo apt update
+  sudo apt install git -y
+be
+echo 'git has been installed'
+
 # enable bbr
 if ! echo "net.core.default_qdisc=fq" | grep -q "net.core.default_qdisc=fq" /etc/sysctl.conf ; then
     echo "enabling bbr step 1"
@@ -13,19 +22,33 @@ fi
 
 sysctl -p
 
-# install xray x2
+# install xray 1.8
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --version 1.8.0 -u root
 
+#download geo files
+wget -O /usr/local/share/xray/iran.dat https://github.com/bootmortis/iran-hosted-domains/releases/latest/download/iran.dat
+wget -O /usr/local/share/xray/dlc.dat https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat
 
+# isntall  nginx
 apt install nginx -y
 
+#install openssl
+echo "Install OpenSSL"
+sudo apt-get update && sudo apt-get install openssl
+
+#install python 3.1o
 apt install software-properties-common -y
 
 add-apt-repository ppa:deadsnakes/ppa -y
 
 apt update
 
-apt install python3.10 -y;
+apt install python3.10 -y
+
+#install screen
+apt install screen -y
+
+#downlaod python script
 
 # run python cli
 if test -f "./reality.py" ; then
