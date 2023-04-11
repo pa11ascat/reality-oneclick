6aaac3c3473ad977b64a38eb066ce2e7ce8c2df2
Mohammad Massoudi <m.massoudi32@gmail.com>
2023-04-11 03:35:23 +0330

    remove grep

--- setup.sh
+++ setup.sh
@@ -16,17 +16,8 @@ sysctl -p
 # install xray x2
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --version 1.8.0 -u root
 
-if ! echo "Xray 1.8.0" | grep -q "Xray 1.8.0" `xray -version` ; then
-    echo "ERROR: Unable to install Xray 1.8.0"
-    exit
-fi
 
 apt install nginx -y
-
-if ! echo "nginx version:" | grep -q "nginx version:"git `nginx -v` ; then
-    echo "ERROR: Unable to install nginx"
-    exit
-fi
 
 apt install python3 -y;
 
